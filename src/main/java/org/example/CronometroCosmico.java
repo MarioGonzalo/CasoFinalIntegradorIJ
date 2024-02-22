a033c6a1d85136878b8a86fcb37427cfc8b91ea6
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2024-02-22 11:31:03 +0100

    Clase Antonio

---
+++ src/main/java/org/example/CronometroCosmico.java
@@ -0,0 +1,50 @@
+package org.example;
+import java.util.Date;
+
+public class CronometroCosmico {
+    Date fechaTierra;
+    Date fechaOtroPlaneta;
+
+    static long FC = 2; // Factor de conversión
+
+    public CronometroCosmico() {
+        fechaTierra = new Date();
+        long fnp = this.fechaTierra.getTime();
+        fechaOtroPlaneta = new Date((long) (fnp * CronometroCosmico.getFC()));
+    }
+
+    public Date getFechaTierra() {
+        return fechaTierra;
+    }
+
+    public Date getFechaOtroPlaneta() {
+        return fechaOtroPlaneta;
+    }
+
+    public void setFechaTierra(Date fechaTierra) {
+        this.fechaTierra = fechaTierra;
+    }
+
+    public void setFechaOtroPlaneta(Date fechaOtroPlaneta) {
+        this.fechaOtroPlaneta = fechaOtroPlaneta;
+    }
+
+    public static double getFC() {
+        return FC;
+    }
+
+    public static void setFC(long FC) {
+        CronometroCosmico.FC = FC;
+    }
+
+    @Override
+    public String toString() {
+        return "CronometroCosmico{" +
+                "fechaTierra=" + fechaTierra +
+                "\n, fechaOtroPlaneta=" + fechaOtroPlaneta +
+                '}';
+    }
+
+
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Date myDate1 = new Date();
+
+        String myString = DateFormat.getDateInstance().format(myDate1);
+
+        System.out.println("Fecha1.0: " + myString);
+        System.out.println("Fecha1.1: " + myDate1.toString());
+
+        long msl = System.currentTimeMillis();
+        Date myDate2 = new Date(msl);
+
+        System.out.println("Fecha2: " + myDate2.toString());
+
+        CronometroCosmico cc = new CronometroCosmico();
+        System.out.println("cc: " + cc.toString());
+
+        System.out.println("Max Entero: " + Integer.MAX_VALUE);
+
     }
 }
