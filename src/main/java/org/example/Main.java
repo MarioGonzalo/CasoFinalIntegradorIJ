b191b6e8e299fd99f7ba7840eb0ab9789dacbdac
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2024-02-24 22:26:13 +0100

    Menú selección completo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,44 @@ public class Main {
     public static void main(String[] args) {
         boolean seguir = true;
         Scanner sc = new Scanner(System.in);
+        int opcion = 0;
         while(seguir) {
-
-            CronometroCosmico cc = new CronometroCosmico();
-            System.out.println(cc.toString());
+            System.out.println("Qué quiere hacer?\n" +
+                    "1. Calcular la fecha actual en otro planeta\n" +
+                    "2. Observar los recursos y suministros\n" +
+                    "3. Ver el sistema de alerta y monitoreo\n" +
+                    "4. Ver el planificador de tareas\n" +
+                    "5. Ver el navegador estelar\n" +
+                    "6. Ver el sistema de comunicación interplanetario\n" +
+                    "7. Salir");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    CronometroCosmico cc = new CronometroCosmico();
+                    System.out.println(cc.toString());
+                    break;
+                case 2:
+                    System.out.println("Recursos y suministros");
+                    break;
+                case 3:
+                    System.out.println("Sistema de alerta y monitoreo");
+                    break;
+                case 4:
+                    System.out.println("Planificador de tareas");
+                    break;
+                case 5:
+                    System.out.println("Navegador estelar");
+                    break;
+                case 6:
+                    System.out.println("Sistema de comunicación interplanetario");
+                    break;
+                case 7:
+                    seguir = false;
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
             System.out.println("Desea terminar el programa? (false para terminar, true para continuar)");
             seguir = sc.nextBoolean();
 
